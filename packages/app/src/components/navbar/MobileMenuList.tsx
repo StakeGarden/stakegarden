9de34dcadd0803691d3b55e367eed0ce618a6e56
Leonardo Berteotti <berteotti2@gmail.com>
2023-07-21 23:23:05 +0100

    feat: create navbar without logo (#1)

---
+++ packages/app/src/components/navbar/MobileMenuList.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { Icon } from "@/src/ui";
+
+interface MobileMenuListProps {
+  open: boolean;
+  toggle: () => void;
+}
+
+export default function MobileMenuList({ open, toggle }: MobileMenuListProps) {
+  if (!open) return null;
+
+  return (
+    <>
+      <div
+        className="fixed bottom-0 left-0 right-0 top-nav-height bg-gray-alpha-75"
+        onClick={toggle}
+      ></div>
+      <div className="absolute left-0 w-full gap-2 px-6 py-2 border-b border-solid rounded-lg bg-surface-25 top-nav-height border-surface-75">
+        <Link
+          href="/stacks"
+          className="flex items-center py-3 text-em-med"
+          onClick={toggle}
+        >
+          <Icon name="blocks" alt="My gardens" size={18} />
+          <span className="ml-4">My gardens</span>
+        </Link>
+      </div>
+    </>
+  );
+}
