b49cd31b79c95f0240eef4a103b56e55e1a0ef19
Diogo Ferreira <diogomf@hey.com>
2023-07-22 00:37:19 +0200

    feat: add portfolios page

---
+++ packages/app/src/app/portfolios/page.tsx
@@ -0,0 +1,14 @@
+import { DisplayText, HeadingText, TitleText } from "@/src/ui";
+
+export default function Portfolios() {
+  return (
+    <main className="py-32">
+      <div className="space-y-4">
+        <div className="max-w-5xl mx-auto space-y-4 text-center">
+          <DisplayText>Staking gardens</DisplayText>
+          <HeadingText>Your garden. Your rules</HeadingText>
+        </div>
+      </div>
+    </main>
+  );
+}
