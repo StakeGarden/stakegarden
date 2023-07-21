eefdc81182d6a34b541ad9ba48571b736e63e10d
Diogo Ferreira <diogomf@hey.com>
2023-07-22 00:25:37 +0200

    feat: add text components

--- packages/app/src/app/page.tsx
+++ packages/app/src/app/page.tsx
@@ -1,12 +1,25 @@
-import Image from "next/image";
-import { Button } from "@/src/ui";
+import { ButtonLink, DisplayText, TitleText } from "@/src/ui";
 import { ConnectButton } from "../components/ConnectButton";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="">
+      <div className="">
         <ConnectButton />
+        <div className="space-y-4">
+          <div className="max-w-5xl mx-auto space-y-4 text-center">
+            <DisplayText>
+              Stake your ETH in the most diversified gardens
+            </DisplayText>
+            <TitleText size={2}>
+              The garden is staked eth portfolios that are fully collateralize.
+              Or you can create your own.{" "}
+            </TitleText>
+          </div>
+          <ButtonLink href="/portfolios" width="fit" className="mx-auto">
+            Explore Portfolios
+          </ButtonLink>
+        </div>
       </div>
     </main>
   );
--- packages/app/src/ui/index.ts
+++ packages/app/src/ui/index.ts
@@ -1,2 +1,3 @@
 export * from "./buttons";
 export * from "./Icon";
+export * from "./text";
