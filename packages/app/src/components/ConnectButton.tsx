a0823daecf69ad28ca648b8439513778bd867b15
Berteotti <berteotti2@gmail.com>
2023-07-21 23:32:12 +0200

    feat: disconnect wallet listener

--- packages/app/src/components/ConnectButton.tsx
+++ packages/app/src/components/ConnectButton.tsx
@@ -5,7 +5,7 @@ import { connectWallet } from "../metamask";
 import { Button } from "../ui";
 
 export const ConnectButton = () => {
-  const [account, setAccount] = useState<string>();
+  const [account, setAccount] = useState<string | undefined>();
   const [isConnected, setIsConnected] = useState(false);
 
   useEffect(() => {
@@ -21,6 +21,26 @@ export const ConnectButton = () => {
 
     if (isConnected && !account) {
       getAddress();
+    }
+  });
+
+  useEffect(() => {
+    // Note that this event is emitted on page load.
+    // If the array of accounts is non-empty, you're already
+    // connected.
+    window.ethereum?.on("accountsChanged", handleAccountsChanged);
+
+    // eth_accounts always returns an array.
+    function handleAccountsChanged(accounts: any) {
+      if (accounts.length === 0) {
+        // MetaMask is locked or the user has not connected any accounts.
+        console.log("Please connect to MetaMask.");
+        setAccount(undefined);
+        setIsConnected(false);
+      } else if (accounts[0] !== account) {
+        // Reload your interface with accounts[0].
+        setAccount(accounts[0]);
+      }
     }
   });
 
