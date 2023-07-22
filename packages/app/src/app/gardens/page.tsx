a32ae059853e60fb36893574deb25b7fd1a565b2
Diogo Ferreira <diogomf@hey.com>
2023-07-23 01:53:20 +0200

    update gardens page design

--- packages/app/src/app/gardens/page.tsx
+++ packages/app/src/app/gardens/page.tsx
@@ -1,5 +1,11 @@
 import { BodyText, ButtonLink, HeadingSection, TitleText } from "@/src/ui";
 
+const tokenImages = [
+  "https://tokens.1inch.io/0xae7ab96520de3a18e5e111b5eaab095312d7fe84.png",
+  "https://tokens.1inch.io/0xae78736cd615f374d3085123a210448e74fc6393.png"
+];
+const tokensArrayETH = ["stETH", "rETH"];
+
 export default function Gardens() {
   return (
     <main>
@@ -10,53 +16,68 @@ export default function Gardens() {
           buttonText="Create Portfolio"
           href="/gardens/create"
         />
-        <div className="p-5 space-y-8 bg-white border shadow-lg rounded-2xl border-surface-25">
-          <div className="flex justify-between">
-            <TitleText size={2}>SG</TitleText>
-            <ButtonLink
-              action="tertiary"
-              size="sm"
-              href="/gardens/garden?address=0x123456789"
+        <div className="space-y-6 md:space-y-12">
+          {[1, 2].map(index => (
+            <div
+              key={index}
+              className="p-5 space-y-8 bg-white border shadow-lg rounded-2xl border-surface-50"
             >
-              Stake
-            </ButtonLink>
-          </div>
-          <BodyText>
-            Stake any amount of ETH, get daily staking rewards and use your
-            stETH across the DeFi ecosystem and L2.
-          </BodyText>
-          <div className="space-y-2">
-            <div className="flex justify-between">
-              <BodyText>Lido</BodyText>
-              <BodyText>45%</BodyText>
+              <div className="flex justify-between">
+                <TitleText size={2}>SG</TitleText>
+                <ButtonLink
+                  action="tertiary"
+                  size="sm"
+                  href="/gardens/garden?address=0x4df5aae5b1acdd48a4c2052e3f7d0efaddcb3d8c"
+                >
+                  Stake
+                </ButtonLink>
+              </div>
+              <div className="space-y-4 divide-y divide-surface-25">
+                <div className="flex justify-between">
+                  <div className="flex items-center space-x-5">
+                    <img
+                      src={tokenImages[0]}
+                      className="w-8 h-8 border rounded-full border-surface-75"
+                      alt="token logo"
+                    />
+                    <BodyText>{tokensArrayETH[0]}</BodyText>
+                  </div>
+                  <BodyText>45%</BodyText>
+                </div>
+                <div className="flex justify-between">
+                  <div className="flex items-center space-x-5">
+                    <img
+                      src={tokenImages[1]}
+                      className="w-8 h-8 border rounded-full border-surface-75"
+                      alt="token logo"
+                    />
+                    <BodyText>{tokensArrayETH[1]}</BodyText>
+                  </div>
+                  <BodyText>55%</BodyText>
+                </div>
+              </div>
+              <div className="space-y-2">
+                <div className="flex justify-between px-4 py-2 rounded-lg bg-surface-25">
+                  <BodyText className="text-em-med">Variable APY</BodyText>
+                  <BodyText>4.23%</BodyText>
+                </div>
+                <div className="flex justify-between px-4 py-2 rounded-lg bg-surface-25">
+                  <BodyText className="text-em-med">Total ETH</BodyText>
+                  <BodyText>23</BodyText>
+                </div>
+                <div className="flex justify-between px-4 py-2 rounded-lg bg-surface-25">
+                  <BodyText className="text-em-med">Unique Owners</BodyText>
+                  <BodyText>74</BodyText>
+                </div>
+                <div className="flex justify-between px-4 py-2 rounded-lg bg-surface-25">
+                  <BodyText className="text-em-med">Contract address</BodyText>
+                  <BodyText>
+                    0x4df5aae5b1acdd48a4c2052e3f7d0efaddcb3d8c
+                  </BodyText>
+                </div>
+              </div>
             </div>
-            <div className="flex justify-between">
-              <BodyText>Lido</BodyText>
-              <BodyText>45%</BodyText>
-            </div>
-            <div className="flex justify-between">
-              <BodyText>Lido</BodyText>
-              <BodyText>45%</BodyText>
-            </div>
-          </div>
-          <div className="space-y-2">
-            <div className="flex justify-between px-4 py-2 bg-surface-75 rounded-xl">
-              <BodyText>Variable APY</BodyText>
-              <BodyText>100%</BodyText>
-            </div>
-            <div className="flex justify-between px-4 py-2 bg-surface-75 rounded-xl">
-              <BodyText>Total ETH</BodyText>
-              <BodyText>30</BodyText>
-            </div>
-            <div className="flex justify-between px-4 py-2 bg-surface-75 rounded-xl">
-              <BodyText>Unique Owners</BodyText>
-              <BodyText>30</BodyText>
-            </div>
-            <div className="flex justify-between px-4 py-2 bg-surface-75 rounded-xl">
-              <BodyText>Contract address</BodyText>
-              <BodyText>0x123456789</BodyText>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </main>
