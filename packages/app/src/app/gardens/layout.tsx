3af1805a67a826f485ea88c8673ec6bc8bf69efa
Diogo Ferreira <diogomf@hey.com>
2023-07-22 17:29:12 +0200

    feat: add bambo

--- packages/app/src/app/gardens/layout.tsx
+++ packages/app/src/app/gardens/layout.tsx
@@ -1,7 +1,16 @@
+import Bambo from "@/public/assets/nouns/bambo.svg";
+
 export default function GardensLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
-  return <section className="max-w-2xl px-4 py-16 mx-auto">{children}</section>;
+  return (
+    <section className="max-w-2xl px-4 py-16 mx-auto">
+      {children}
+      <div className="absolute left-0 -bottom-64 -z-10 opacity-80">
+        <Bambo />
+      </div>
+    </section>
+  );
 }
